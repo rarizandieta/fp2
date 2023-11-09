ca82b57649f49656bff577eae0228ecaa20384ce
Rigoberto Gonzalez <rigonzalez@catedratico.udv.edu.gt>
2023-11-08 19:46:39 -0600

    changing implementation to send emails

--- servidorCorreos.py
+++ servidorCorreos.py
@@ -5,8 +5,8 @@ class Correos:
         self.descuentos = descuentos
 
     def enviarCorreo(self):
-        print("Enviando a enviar correo")
         print("Total pagado: " + str(self.pago))
         print("Total descuentos: " + str(self.descuentos))
         print("correo enviado!!!")
+        print("se agregó esta nueva implementación!!!")
         # aca definir implementacion para enviar corroe electronico
